46e4ccad8e83564ab169eeec3a5ba7015350d0e0
Haisham <mhaisham79@gmail.com>
2026-07-15 21:06:33 +0500

    Enhance CLI documentation with detailed usage instructions and verbosity options

--- src/cli.rs
+++ src/cli.rs
@@ -1,6 +1,19 @@
 use clap::{Parser, Subcommand};
 
+/// A command-line tool for interacting with the Hummingbird development stack.
+///
+/// This tool is designed to be used in conjunction with the Hummingbird development stack.
+/// It provides a simple interface for starting, stopping, and restarting all projects/containers in the stack.
+///
+/// The tool is designed to be run globally or within a specific project.
+/// When run globally, it will apply the specified command to all projects in the stack.
+///
+/// You may specify the project to run the command on by providing the project name as the first argument.
+/// If no project is specified, the tool will attempt to detect the current project based on the current working directory.
+///
+/// You can use the `--verbose` flag to increase the verbosity of the output for debugging purposes.
 #[derive(Debug, Parser)]
+#[clap(version, about)]
 pub struct Cli {
     /// Increase verbosity for debugging purposes.
     #[clap(short, long, action = clap::ArgAction::Count)]
